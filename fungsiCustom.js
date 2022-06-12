552b1a666bfad7f3e3d1b358f5666ea2317d4783
[agung permana] <[agung.ten@gmail.com]>
2022-06-12 18:50:03 +0700

    callback fungsi custom

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,58 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+
+const stepFile1 = (nextStep) => {
+  fs.readFile(file1, 'utf-8', (err, data) => {
+    let error = null;
+    let hasilBaca = [];
+    if (err) {
+      error = err;
+    } else {
+      let result = JSON.parse(data);
+      hasilBaca.push(result.message.split(' ')[1]);
+    }
+    nextStep(error, hasilBaca);
+  });
+}
+
+const stepFile2 = (error, hasilBaca, nextStep) => {
+  fs.readFile(file2, 'utf-8', (err, data) => {
+    if(!error) {
+      if (err) {
+        error = err;
+      } else {
+        let result = JSON.parse(data);
+        hasilBaca.push(result[0].message.split(' ')[1]);
+      }
+    }
+    nextStep(error, hasilBaca);
+  });
+}
+
+const stepFile3 = (error, hasilBaca, nextStep) => {
+  fs.readFile(file3, 'utf-8', (err, data) => {
+    if(!error) {
+      if (err) {
+        error = err;
+      } else {
+        let result = JSON.parse(data);
+        hasilBaca.push(result[0].data.message.split(' ')[1]);
+      }
+    }
+    nextStep(error, hasilBaca); 
+  });
+}
+
+const bacaData = (fnCallBack) => {
+  stepFile1((error1, hasilBaca) => {
+    stepFile2(error1, hasilBaca, (error2, hasilStep2) => {
+      stepFile3(error2, hasilStep2, (error3, hasilStep3) => {
+        fnCallBack(error3, hasilStep3);
+      })
+    })
+  }); 
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
